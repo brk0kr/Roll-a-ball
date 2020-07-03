1fd9018a8d13688856ddc9083409a9d0f59c069c
Berkay Karadeniz <brkykrdnz85@gmai.com>
2020-07-03 19:17:18 +0200

    Roll a Ball
    Added on github

---
+++ Assets/Codes/Control_Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Control_Ball : MonoBehaviour
+{
+    Rigidbody physic;
+    public int speed;
+    int counter = 0;
+    public int countingObjectNumber;
+    public Text counterText;
+    public Text TheEndText;
+
+    void Start()
+    {
+        physic = GetComponent<Rigidbody>();
+
+
+
+ }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(horizontal, 0, vertical);
+        physic.AddForce(vec*speed);
+
+       // Debug.Log("horizontal =" + horizontal+ "vertical =" + vertical );
+    
+    }
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Blocker")
+        {
+            other.gameObject.SetActive(false);
+            counter++;
+            counterText.text = "Counter =  " + counter;
+
+            if (counter== countingObjectNumber)
+            {
+                TheEndText.text = "THE END!";
+            }
+            
+        }
+
+    }
+}
---
+++ Assets/Codes/Control_Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_Camera : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 Distance;
+    void Start()
+    {
+        Distance = transform.position - ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + Distance;
+    }
+}
